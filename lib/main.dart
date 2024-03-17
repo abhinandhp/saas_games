48be283cb1a2672b631c4e186b710012b2b00ab7
Abhinandh P <138770029+abhinandhp@users.noreply.github.com>
2024-03-17 10:07:37 +0530

    updated main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(``
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: IntroToHome(),
